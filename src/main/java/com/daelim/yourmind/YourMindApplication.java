4172395a2b254fa38bb1fde0857ec357f7394cdd
JunSeong_Park <ttotto0608@naver.com>
2023-04-19 15:34:11 +0900

    first commit

---
+++ src/main/java/com/daelim/yourmind/YourMindApplication.java
@@ -0,0 +1,13 @@
+package com.daelim.yourmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourMindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourMindApplication.class, args);
+    }
+
+}
